8fc0c72612078b0838b2c8e5401dbdf594e0c593
nikolavojicic <nikolavojicic@outlook.com>
2020-11-05 19:30:54 +0100

    feature/ASE-18: Add swagger for endpoints

--- src/main/java/com/bloxico/ase/userservice/web/api/AuthenticationApi.java
+++ src/main/java/com/bloxico/ase/userservice/web/api/AuthenticationApi.java
@@ -4,6 +4,10 @@ import com.bloxico.ase.userservice.dto.token.DecodedJwtDto;
 import com.bloxico.ase.userservice.web.model.auth.AuthenticationRequest;
 import com.bloxico.ase.userservice.web.model.auth.AuthenticationResponse;
 import com.bloxico.ase.userservice.web.model.auth.BlacklistRequest;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
@@ -11,11 +15,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.validation.Valid;
 
+@Api(value = "authentication")
 public interface AuthenticationApi {
+
 
     String AUTHENTICATE_ENDPOINT = "/authenticate",
            BLACKLIST_ENDPOINT    = "/ase/jwt/blacklist";
 
+
+    @ApiOperation(value = "Creates a new JWT for user.")
+    @ApiResponses({
+            @ApiResponse(
+                    code = 200,
+                    message = "JWT is created successfully."),
+            @ApiResponse(
+                    code = 404,
+                    message = "User is not found.")})
     @PostMapping(
             value = AUTHENTICATE_ENDPOINT,
             consumes = {"application/json"},
@@ -25,6 +40,12 @@ public interface AuthenticationApi {
             (@RequestBody @Valid
              AuthenticationRequest body);
 
+
+    @ApiOperation(value = "Blacklists a JWT.")
+    @ApiResponses({
+            @ApiResponse(
+                    code = 200,
+                    message = "JWT is blacklisted successfully.")})
     @PostMapping(
             value = BLACKLIST_ENDPOINT,
             consumes = {"application/json"},
@@ -35,5 +56,6 @@ public interface AuthenticationApi {
              DecodedJwtDto decodedJwt,
              @RequestBody @Valid
              BlacklistRequest request);
+
 
 }
