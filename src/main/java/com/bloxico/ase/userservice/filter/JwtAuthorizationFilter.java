5041032f2f1ec349b3872e9120c80b17c543b920
Sekula <sekuloskimilos@gmail.com>
2020-11-06 13:56:49 +0100

    feature/ASE-18: - JWT Filter throws an error if authorization fails

--- src/main/java/com/bloxico/ase/userservice/filter/JwtAuthorizationFilter.java
+++ src/main/java/com/bloxico/ase/userservice/filter/JwtAuthorizationFilter.java
@@ -1,20 +1,14 @@
 package com.bloxico.ase.userservice.filter;
 
-import com.bloxico.ase.userservice.entity.user.Role;
 import com.bloxico.ase.userservice.service.token.IJwtService;
-import com.bloxico.ase.userservice.web.api.AuthenticationApi;
 import com.bloxico.ase.userservice.web.error.ErrorCodes;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
+import com.bloxico.userservice.web.model.ApiError;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
-import org.springframework.security.oauth2.provider.authentication.BearerTokenExtractor;
-import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationManager;
-import org.springframework.security.oauth2.provider.authentication.TokenExtractor;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -22,9 +16,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Map;
-
-import static java.util.Map.entry;
 
 public class JwtAuthorizationFilter extends OncePerRequestFilter {
 
@@ -41,16 +32,15 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain chain)
-            throws ServletException, IOException
-    {
+            throws ServletException, IOException {
 
-        var uri = request.getRequestURI().replace(request.getContextPath(), "");
-//        if (URI_PERMISSION_MAP.containsKey(uri)) {
-        if (true) {
-            var header = request.getHeader("Authorization");
-            if (header == null || !header.startsWith("Bearer "))
-                throw ErrorCodes.Jwt.INVALID_TOKEN.newException();
+        var header = request.getHeader("Authorization");
+        if (header == null || !header.startsWith("Bearer ")) {
+            raiseException(response);
+            return;
+        }
 
+        try {
             var jwt = header.substring(7);
             var decodedJwt = jwtService.verifyToken(jwt);
 
@@ -60,8 +50,20 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
             request.setAttribute("decodedJwt", decodedJwt);
 
             SecurityContextHolder.getContext().setAuthentication(oAuth2Authentication);
+
+            chain.doFilter(request, response);
+        } catch (Exception e) {
+            raiseException(response);
         }
-        chain.doFilter(request, response);
+    }
+
+    private void raiseException(HttpServletResponse response)
+            throws IOException {
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        ApiError apiError = new ApiError(HttpStatus.FORBIDDEN, ErrorCodes.Jwt.INVALID_TOKEN.getCode());
+        byte[] body = new ObjectMapper().writeValueAsBytes(apiError);
+        response.getOutputStream().write(body);
     }
 
 }
