7b0ba68108a8aefed64eda534a8543a59e1e8fca
nikolavojicic <nikolavojicic@outlook.com>
2020-12-03 19:14:37 +0100

    feature/ASE-31: Fix missing annotations for request model classes

--- src/main/java/com/bloxico/ase/userservice/web/model/user/DisableUserRequest.java
+++ src/main/java/com/bloxico/ase/userservice/web/model/user/DisableUserRequest.java
@@ -1,11 +1,17 @@
 package com.bloxico.ase.userservice.web.model.user;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Value;
 
 import javax.validation.constraints.NotNull;
 
+import static lombok.AccessLevel.PRIVATE;
+
 @Value
+@AllArgsConstructor
+@NoArgsConstructor(force = true, access = PRIVATE)
 public class DisableUserRequest {
 
     @NotNull
