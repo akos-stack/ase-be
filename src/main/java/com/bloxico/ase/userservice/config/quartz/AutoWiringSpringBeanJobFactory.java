ca7c1e99876fe25989f87abe78a928449fba5c0c
nikolavojicic <nikolavojicic@outlook.com>
2020-12-14 03:33:19 +0100

    feature/ASE-48: Add Quartz config and schedule expired token deletion job

--- src/main/java/com/bloxico/ase/userservice/config/quartz/AutoWiringSpringBeanJobFactory.java
+++ src/main/java/com/bloxico/ase/userservice/config/quartz/AutoWiringSpringBeanJobFactory.java
@@ -1,7 +1,6 @@
-package com.bloxico.userservice.util.quartz;
+package com.bloxico.ase.userservice.config.quartz;
 
 import org.quartz.spi.TriggerFiredBundle;
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -10,21 +9,21 @@ import org.springframework.scheduling.quartz.SpringBeanJobFactory;
 /**
  * Adds auto-wiring support to quartz jobs.
  *
- * @see "https://gist.github.com/jelies/5085593"
+ * @see <a href="https://gist.github.com/jelies/5085593">
+ * https://gist.github.com/jelies/5085593
+ * </a>
  */
 public final class AutoWiringSpringBeanJobFactory extends SpringBeanJobFactory implements ApplicationContextAware {
 
     private transient AutowireCapableBeanFactory beanFactory;
 
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-
-        beanFactory = applicationContext.getAutowireCapableBeanFactory();
+    public void setApplicationContext(ApplicationContext context) {
+        beanFactory = context.getAutowireCapableBeanFactory();
     }
 
     @Override
-    protected Object createJobInstance(final TriggerFiredBundle bundle) throws Exception {
-
-        final Object job = super.createJobInstance(bundle);
+    protected Object createJobInstance(TriggerFiredBundle bundle) throws Exception {
+        var job = super.createJobInstance(bundle);
         beanFactory.autowireBean(job);
         return job;
     }
