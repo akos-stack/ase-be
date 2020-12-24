54e98e61de86721a2bc4e21d2e936f967d42cd80
Ivana Bašić <ivana.basic@addiko.com>
2020-12-24 22:09:19 +0100

    Disable user and blacklist token tests moved to Management Tests

---
+++ src/test/java/com/bloxico/ase/userservice/facade/impl/UserManagementFacadeImplTest.java
@@ -0,0 +1,77 @@
+package com.bloxico.ase.userservice.facade.impl;
+
+import com.bloxico.ase.testutil.AbstractSpringTest;
+import com.bloxico.ase.testutil.MockUtil;
+import com.bloxico.ase.userservice.exception.UserProfileException;
+import com.bloxico.ase.userservice.service.token.impl.TokenBlacklistServiceImpl;
+import com.bloxico.ase.userservice.service.user.impl.UserProfileServiceImpl;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.util.Set;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.annotation.DirtiesContext.MethodMode.BEFORE_METHOD;
+
+public class UserManagementFacadeImplTest extends AbstractSpringTest {
+
+    @Autowired
+    private MockUtil mockUtil;
+
+    @Autowired
+    private TokenBlacklistServiceImpl tokenBlacklistService;
+
+    @Autowired
+    private UserProfileServiceImpl userProfileService;
+
+    @Autowired
+    private UserManagementFacadeImpl userManagementFacade;
+
+    @Test(expected = UserProfileException.class)
+    public void disableUser_notFound() {
+        var principalId = mockUtil.savedAdmin().getId();
+        userManagementFacade.disableUser(-1, principalId);
+    }
+
+    @Test
+    @DirtiesContext(methodMode = BEFORE_METHOD) // clear cache
+    public void disableUser() {
+        var principalId = mockUtil.savedAdmin().getId();
+        var user = mockUtil.savedUserProfile();
+        var userId = user.getId();
+        var tokens = Set.of(
+                mockUtil.savedOauthToken(user.getEmail()).getTokenId(),
+                mockUtil.savedOauthToken(user.getEmail()).getTokenId(),
+                mockUtil.savedOauthToken(user.getEmail()).getTokenId());
+        assertEquals(Set.of(), tokenBlacklistService.blacklistedTokens());
+        assertTrue(userProfileService.findUserProfileById(userId).getEnabled());
+        userManagementFacade.disableUser(userId, principalId);
+        assertEquals(tokens, tokenBlacklistService.blacklistedTokens());
+        assertFalse(userProfileService.findUserProfileById(userId).getEnabled());
+    }
+
+    @Test(expected = UserProfileException.class)
+    public void blacklistTokens_notFound() {
+        var principalId = mockUtil.savedAdmin().getId();
+        userManagementFacade.blacklistTokens(-1, principalId);
+    }
+
+    @Test
+    @DirtiesContext(methodMode = BEFORE_METHOD) // clear cache
+    public void blacklistTokens() {
+        var principalId = mockUtil.savedAdmin().getId();
+        var user = mockUtil.savedUserProfile();
+        var userId = user.getId();
+        var tokens = Set.of(
+                mockUtil.savedOauthToken(user.getEmail()).getTokenId(),
+                mockUtil.savedOauthToken(user.getEmail()).getTokenId(),
+                mockUtil.savedOauthToken(user.getEmail()).getTokenId());
+        assertEquals(Set.of(), tokenBlacklistService.blacklistedTokens());
+        assertTrue(userProfileService.findUserProfileById(userId).getEnabled());
+        userManagementFacade.blacklistTokens(userId, principalId);
+        assertEquals(tokens, tokenBlacklistService.blacklistedTokens());
+        assertTrue(userProfileService.findUserProfileById(userId).getEnabled());
+    }
+
+}
---
+++ src/test/java/com/bloxico/ase/userservice/web/api/UserManagementApiTest.java
@@ -0,0 +1,145 @@
+package com.bloxico.ase.userservice.web.api;
+
+import com.bloxico.ase.testutil.AbstractSpringTest;
+import com.bloxico.ase.testutil.MockUtil;
+import com.bloxico.ase.userservice.repository.token.TokenRepository;
+import com.bloxico.ase.userservice.web.error.ErrorCodes;
+import com.bloxico.ase.userservice.web.model.user.BlacklistTokensRequest;
+import com.bloxico.ase.userservice.web.model.user.DisableUserRequest;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.transaction.annotation.Transactional;
+
+import static com.bloxico.ase.testutil.MockUtil.ERROR_CODE;
+import static com.bloxico.ase.userservice.web.api.UserManagementApi.USER_BLACKLIST_TOKENS;
+import static com.bloxico.ase.userservice.web.api.UserManagementApi.USER_DISABLE;
+import static com.bloxico.ase.userservice.web.api.UserProfileApi.MY_PROFILE_ENDPOINT;
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD;
+import static org.springframework.transaction.annotation.Propagation.NOT_SUPPORTED;
+
+// Because RestAssured executes in another transaction
+@Transactional(propagation = NOT_SUPPORTED)
+@DirtiesContext(classMode = AFTER_EACH_TEST_METHOD)
+public class UserManagementApiTest extends AbstractSpringTest {
+
+    @Autowired
+    private MockUtil mockUtil;
+
+    @Autowired
+    private TokenRepository tokenRepository;
+
+    @Test
+    public void disableUser_404_userNotFound() {
+        given()
+                .header("Authorization", mockUtil.doAdminAuthentication())
+                .contentType(JSON)
+                .body(new DisableUserRequest(-1L))
+                .when()
+                .post(API_URL + USER_DISABLE)
+                .then()
+                .assertThat()
+                .statusCode(404)
+                .body(ERROR_CODE, is(ErrorCodes.User.USER_NOT_FOUND.getCode()));
+    }
+
+    @Test
+    public void disableUser_200_ok() {
+        var registration = mockUtil.doConfirmedRegistration();
+        var userToken = mockUtil.doAuthentication(registration);
+        // User can access secured endpoints
+        given()
+                .header("Authorization", userToken)
+                .when()
+                .get(API_URL + MY_PROFILE_ENDPOINT)
+                .then()
+                .assertThat()
+                .statusCode(200);
+        // Disable user
+        given()
+                .header("Authorization", mockUtil.doAdminAuthentication())
+                .contentType(JSON)
+                .body(new DisableUserRequest(registration.getId()))
+                .when()
+                .post(API_URL + USER_DISABLE)
+                .then()
+                .assertThat()
+                .statusCode(200);
+        // User can't access secured endpoints with blacklisted token
+        given()
+                .header("Authorization", userToken)
+                .when()
+                .get(API_URL + MY_PROFILE_ENDPOINT)
+                .then()
+                .assertThat()
+                .statusCode(403);
+        // User can't authenticate and obtain a new token
+        mockUtil.doAuthenticationRequest(registration)
+                .then()
+                .assertThat()
+                .statusCode(400)
+                .body(
+                        "error", is("invalid_grant"),
+                        "error_description", is("User is disabled"));
+    }
+
+    @Test
+    public void blacklistTokens_404_userNotFound() {
+        given()
+                .header("Authorization", mockUtil.doAdminAuthentication())
+                .contentType(JSON)
+                .body(new BlacklistTokensRequest(-1L))
+                .when()
+                .post(API_URL + USER_BLACKLIST_TOKENS)
+                .then()
+                .assertThat()
+                .statusCode(404)
+                .body(ERROR_CODE, is(ErrorCodes.User.USER_NOT_FOUND.getCode()));
+    }
+
+    @Test
+    public void blacklistTokens_200_ok() {
+        var registration = mockUtil.doConfirmedRegistration();
+        var userToken = mockUtil.doAuthentication(registration);
+        // User can access secured endpoints
+        given()
+                .header("Authorization", userToken)
+                .when()
+                .get(API_URL + MY_PROFILE_ENDPOINT)
+                .then()
+                .assertThat()
+                .statusCode(200);
+        // Blacklist user's tokens
+        given()
+                .header("Authorization", mockUtil.doAdminAuthentication())
+                .contentType(JSON)
+                .body(new BlacklistTokensRequest(registration.getId()))
+                .when()
+                .post(API_URL + USER_BLACKLIST_TOKENS)
+                .then()
+                .assertThat()
+                .statusCode(200);
+        // User can't access secured endpoints with blacklisted token
+        given()
+                .header("Authorization", userToken)
+                .when()
+                .get(API_URL + MY_PROFILE_ENDPOINT)
+                .then()
+                .assertThat()
+                .statusCode(403);
+        // User can authenticate and obtain a new token
+        var newUserToken = mockUtil.doAuthentication(registration);
+        // User can access secured endpoints with the new token
+        given()
+                .header("Authorization", newUserToken)
+                .when()
+                .get(API_URL + MY_PROFILE_ENDPOINT)
+                .then()
+                .assertThat()
+                .statusCode(200);
+    }
+
+}
