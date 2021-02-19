ac8eee2c96433d4305fca43124e541ba45d58c2e
Ivana Bašić <icabejzik@gmail.com>
2021-02-19 18:27:29 +0100

    feature/ASE-107: Removed list - stackoverflow exception

--- src/main/java/com/bloxico/ase/userservice/dto/entity/artwork/ArtworkGroupDto.java
+++ src/main/java/com/bloxico/ase/userservice/dto/entity/artwork/ArtworkGroupDto.java
@@ -1,12 +1,8 @@
 package com.bloxico.ase.userservice.dto.entity.artwork;
 
-import com.bloxico.ase.userservice.entity.artwork.Artwork;
 import com.bloxico.ase.userservice.entity.artwork.ArtworkGroup;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @Data
 public class ArtworkGroupDto {
@@ -16,7 +12,4 @@ public class ArtworkGroupDto {
 
     @JsonProperty("status")
     private ArtworkGroup.Status status;
-
-    @JsonProperty("artworks")
-    private Set<Artwork> artworks = new HashSet<>();
 }
