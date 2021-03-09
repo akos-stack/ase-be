d8305c809b08d26522fe93ebf6d1e7c60ca520f2
Danijel Mitrović <rodjenihm@outlook.com>
2021-03-09 10:20:55 +0100

    feature/ASE-118: Create Config entity and related repository and table in the database

---
+++ src/main/java/com/bloxico/ase/userservice/entity/config/Config.java
@@ -0,0 +1,31 @@
+package com.bloxico.ase.userservice.entity.config;
+
+import com.bloxico.ase.userservice.entity.BaseEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.Table;
+
+import static javax.persistence.EnumType.STRING;
+
+@Data
+@EqualsAndHashCode(of = {"type", "value"}, callSuper = false)
+@Entity
+@Table(name = "configs")
+public class Config extends BaseEntity {
+
+    public enum Type {
+        QUOTATION_PACKAGE_MIN_SIZE
+    }
+
+    @Column(name = "type", unique = true)
+    @Enumerated(STRING)
+    private Type type;
+
+    @Column(name = "value")
+    private String value;
+
+}
