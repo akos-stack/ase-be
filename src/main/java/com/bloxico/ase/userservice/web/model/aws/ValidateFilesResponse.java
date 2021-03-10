6a10bb1373402ab157eafef3f005cba945557432
nikolavojicic <nikolavojicic@outlook.com>
2021-03-10 18:26:58 +0100

    feature/ASE-120-2: Forgot response class

---
+++ src/main/java/com/bloxico/ase/userservice/web/model/aws/ValidateFilesResponse.java
@@ -0,0 +1,20 @@
+package com.bloxico.ase.userservice.web.model.aws;
+
+import com.bloxico.ase.userservice.web.error.ErrorCodes;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import java.util.Map;
+import java.util.Set;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@Value
+@AllArgsConstructor
+@NoArgsConstructor(force = true, access = PRIVATE)
+public class ValidateFilesResponse {
+
+    @JsonProperty("errors")
+    Map<String, Set<ErrorCodes.AmazonS3>> errors;
+
+}
